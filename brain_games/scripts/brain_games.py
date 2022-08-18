d1617b105f4912a5c1e7d40c1417f47910e68ee2
nurguntimofeev <squirrelbrain@mail.ru>
2022-08-18 12:01:02 +0900

    version 0.3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+from brain_games.scripts.cli import welcome_user
+
 def greet():
     print(f'Welcome to the Brain Games!')
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
