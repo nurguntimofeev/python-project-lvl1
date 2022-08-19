8f9bdf38f9ad9531e8d4fc0bdfc4fd0abdd2a7f9
nurguntimofeev <squirrelbrain@mail.ru>
2022-08-19 16:22:38 +0900

    v0.5 add brain_even.py

---
+++ brain_games/games_logics/brain_even_logic.py
@@ -0,0 +1,25 @@
+import prompt
+from random import randint
+
+
+def is_even():
+    name = prompt.string('May I have your name? ')
+    print('Answer "yes" if the number is even, otherwise "no".')
+    right_answers = 0
+    while right_answers != 3:
+        random_number = randint(1, 1000)
+        print(f'Question: {random_number}')
+        answer = prompt.string('Your answer: ')
+        if random_number % 2 == 0:
+            result = 'yes'
+        elif random_number % 2 != 0:
+            result = 'no'
+        if answer == result:
+            right_answers += 1
+            print('Correct!')
+        else:
+            print(f'''"{answer}" is wrong answer ;( Correct answer was "{result}".
+Let's try again, {name}!''')
+            right_answers = 0
+            return 0
+    print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games_logics.brain_even_logic import is_even
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    is_even()
+
+
+if __name__ == '__main__':
+    main()
